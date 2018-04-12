e83525413a97868c1c132563cb1ff6c956359cda
Tom <tomasjose1201@gmail.com>
2018-04-12 11:45:39 -0300

    changed SB Admin 2 to SB Admin

--- src/main/java/br/ufpr/tads/tcc/spge/controller/LoginController.java
+++ src/main/java/br/ufpr/tads/tcc/spge/controller/LoginController.java
@@ -55,7 +55,7 @@ public class LoginController extends HttpServlet {
             HttpSession session = request.getSession();
             session.setAttribute("email", result.getEmail());
             session.setAttribute("senha", result.getSenha());
-            RequestDispatcher rd = getServletContext().getRequestDispatcher("/user/pages/index.html");
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/user/index.html");
             rd.forward(request, response);
         } else {
             //Redireciona para index.jsp, passando uma mensagem como parâmetro
